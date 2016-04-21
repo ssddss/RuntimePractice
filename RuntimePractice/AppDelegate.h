f37d1949f3b813b62ebdc80e98e155c797dc13fb
yurongde <359067441@qq.com>
2016-04-21 16:10:39 +0800

    Initial Commit

---
+++ RuntimePractice/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  RuntimePractice
+//
+//  Created by yurongde on 16/4/21.
+//  Copyright © 2016年 yurongde. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ RuntimePractice/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  RuntimePractice
+//
+//  Created by yurongde on 16/4/21.
+//  Copyright © 2016年 yurongde. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
